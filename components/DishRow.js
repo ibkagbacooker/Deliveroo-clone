a4fd7104ad50939f242d41ac9425f745697e0ad1
ibukunoluwa <ibukunogunfowokan@gmail.com>
2023-09-02 20:11:38 +0100

    Ad and remove from basket functionality 8:11pm

--- components/DishRow.js
+++ components/DishRow.js
@@ -2,10 +2,23 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import { urlFor } from '../sanity';
 import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToBasket, removeFromBasket, selectBasketItems, selectBasketItemsWithId } from '../features/basketSlice';
 
 const DishRow = ({id, name, description, price, image}) => {
     // animation for quantity
     const[isPressed, setIsPressed] = useState(false);
+    const items = useSelector((state) => selectBasketItemsWithId(state, id));
+    const dispatch = useDispatch();
+    const addItemToBasket = () =>{
+        dispatch(addToBasket({id, name, description, price, image}));
+    }
+
+    const removeItemFromBasket = () =>{
+        if (!items.length >0) return;
+
+        dispatch(removeFromBasket({id}));
+    };
 
   return (
     <>
@@ -37,16 +50,18 @@ const DishRow = ({id, name, description, price, image}) => {
     {isPressed &&(
         <View classname = "bg-white px-4">
             <View className = "flex-row items-center space-x-2 pb-3 ">
-                <TouchableOpacity>
+                <TouchableOpacity onPress={removeItemFromBasket}>
                     <MinusCircleIcon
                         // color = {items.length > 0 ? "#00ccbb": "gray"}
                         color = {"#00ccbb"}
                         size={40}
                     />
                 </TouchableOpacity>
-                <Text>0</Text>
+                <Text>{items.length}</Text>
 
-                <TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={addItemToBasket}
+                >
                     <PlusCircleIcon
                         // color = {items.length > 0 ? "#00ccbb": "gray"}
                         size={40}
--- store.js
+++ store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import basketReducer from "./features/basketSlice"
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {
+    basket: basketReducer,
+  },
+});  
